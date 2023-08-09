2c4cac3a04d415fc236a6d40f20636e4fee7b32e
Felix <mimiyoyo1001@gmail.com>
2023-08-09 16:10:15 +0800

    VUE ADD CART

---
+++ EBookStoreVue/src/components/CartAddRemove.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="product" class="input-group plus-minus">
+        <button class="btn btn-outline-secondary " :class="loading ? 'disabled' : ''" @click="addOrRemove(-1)" type="button"
+            id="button-addon1">-</button>
+        <input type="number" v-model="qty" disabled class="form-control form-control-sm" placeholder=""
+            aria-label="Example text with button addon" aria-describedby="button-addon1">
+        <button class="btn btn-outline-secondary" :class="loading ? 'disabled' : ''" @click="addOrRemove(1)" type="button"
+            id="button-addon1">+</button>
+    </div>
+</template>
+    
+<script setup>
+
+</script>
+    
+<style>
+.plus-minus input {
+    max-width: 50px;
+}
+</style>
--- EBookStoreVue/src/views/CartItem.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h2>HelloCartItem</h2>
-</template>
-    
-<script setup>
-
-</script>
-    
-<style></style>
