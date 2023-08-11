36283656e3299a0d8aeea96313e53c7b43dc20ff
鈞復 楊 <ffj471011@gmail.com>
2023-08-11 11:08:46 +0800

    修改BooksRepository的連線字串

---
+++ EBookStoreVue/src/components/Book.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Book111</h2>
+</template>
+  
+  <script setup lang='ts'>
+import { ref, onMounted } from "vue";
+
+const books = ref([]);
+
+const loadBooks = async () => {
+  try {
+    const response = await fetch("https://localhost:7261/api/Books");
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
+    }
+    const datas = await response.json();
+    books.value = datas;
+    console.log(books.value);
+  } catch (error) {
+    console.error("Error loading books:", error);
+  }
+};
+
+onMounted(() => {
+  loadBooks();
+});
+</script>
+  
+  <style>
+</style>
+  
