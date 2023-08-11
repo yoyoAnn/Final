7d9b8b63b8398ffb7f54d9a409444ac8976aa6a3
鈞復 楊 <ffj471011@gmail.com>
2023-08-11 13:34:58 +0800

    新增Book.vue(圖片顯示問題沒解決)

--- EBookStoreVue/src/components/Book.vue
+++ EBookStoreVue/src/components/Book.vue
@@ -1,11 +1,39 @@
 <template>
-  <h2>Book111</h2>
+  <h2>Products Page</h2>
+  <div class="row">
+    <div class="col-3"></div>
+    <div class="col-6"></div>
+    <div class="col-3"></div>
+  </div>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>產品名稱</th>
+        <th>產品價格</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="book in books" :key="book.id">
+        <td>{{ book.bookImage }}</td>
+        <img src="../BooksImage/{{book.bookImage}}" />
+        <td>{{ book.name }}</td>
+        <td>{{ book.price }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
   
-  <script setup lang='ts'>
+<!-- <img src="../BooksImage/9f5cbec6-5394-44ed-ba39-6cdf7da53e89.jpg" /> -->
+
+<!-- <td><img :src="'../BooksImage/' + {{book.bookImage  }}" /></td> -->
+<!-- <img src="../BooksImage/{{book.bookImage}}" /> -->
+
+
+<script setup lang='ts'>
 import { ref, onMounted } from "vue";
 
 const books = ref([]);
+const booksImage = ref([]);
 
 const loadBooks = async () => {
   try {
@@ -15,6 +43,8 @@ const loadBooks = async () => {
     }
     const datas = await response.json();
     books.value = datas;
+
+    // console.log(books.value);
     console.log(books.value);
   } catch (error) {
     console.error("Error loading books:", error);
