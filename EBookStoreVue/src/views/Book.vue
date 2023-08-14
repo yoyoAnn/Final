1ed46bc73ed7e4dd49b3628e15a19d51bbffa962
鈞復 楊 <ffj471011@gmail.com>
2023-08-14 16:49:47 +0800

    完成每日精選

--- EBookStoreVue/src/views/Book.vue
+++ EBookStoreVue/src/views/Book.vue
@@ -1,36 +1,61 @@
 <template>
-  <h2>Products Page</h2>
+  <h2>本日推薦</h2>
   <div class="row">
     <div class="col-3"></div>
     <div class="col-6"></div>
     <div class="col-3"></div>
   </div>
-  <table class="table table-bordered">
-    <thead>
-      <tr>
-        <th>產品名稱</th>
-        <th>產品價格</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="book in books" :key="book.id">
-        <img
-          :src="`src/BooksImage/${book.bookImage}`"
-          style="height: 300px; width: 300px"
-        />
-        <td>{{ book.name }}</td>
-        <td>{{ book.price }}</td>
-      </tr>
-    </tbody>
-  </table>
+
+  <div>
+    <el-row class="button-row">
+      <el-col :span="1">
+        <el-button @click="prevPage" :disabled="currentPage === 1"
+          >上一頁</el-button
+        >
+      </el-col>
+      <el-col :span="18">
+        <div class="card-container">
+          <el-col
+            v-for="(book, index) in displayedBooks"
+            :key="index"
+            :span="6"
+          >
+            <el-card :body-style="{ padding: '0px' }" class="custom-card">
+              <img
+                :src="`src/BooksImage/${book.bookImage}`"
+                style="height: 300px; width: auto; max-width: 100%"
+              />
+              <div style="padding: 14px">
+                <span class="book-title">{{ book.name }}</span>
+                <div class="bottom">
+                  <span class="pricecolor">{{ book.price }} 元</span>
+                  <el-button type="text" class="button">加入購物車</el-button>
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <el-button @click="nextPage" :disabled="currentPage === totalPages"
+          >下一頁</el-button
+        >
+        <i class="fa-solid fa-chevron-right fa-fade"></i>
+      </el-col>
+    </el-row>
+  </div>
 </template>
   
 
 
 <script setup lang='ts'>
-import { ref, onMounted } from "vue";
+import { ref, computed, onMounted } from "vue";
 
 const books = ref([]);
+// const randomBooks = ref([]);
+const currentPage = ref(1);
+const itemsPerPage = 4;
+const numberOfRandomBooks = 10;
 
 const loadBooks = async () => {
   try {
@@ -40,8 +65,6 @@ const loadBooks = async () => {
     }
     const datas = await response.json();
     books.value = datas;
-
-    // console.log(books.value);
     console.log(books.value);
   } catch (error) {
     console.error("Error loading books:", error);
@@ -51,12 +74,39 @@ const loadBooks = async () => {
 onMounted(() => {
   loadBooks();
 });
+
+//隨機選擇指定數量書籍
+
+const getRandomBooks = computed(() => {
+  const shuffledBooks = books.value.sort(() => 0.5 - Math.random());
+  return shuffledBooks.slice(0, numberOfRandomBooks);
+});
+
+//分頁邏輯
+const totalPages = computed(() =>
+  Math.ceil(getRandomBooks.value.length / itemsPerPage)
+);
+
+const displayedBooks = computed(() => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  return getRandomBooks.value.slice(startIndex, endIndex);
+});
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+  }
+};
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++;
+  }
+};
 </script>
   
-<style>
-.book-image {
-  width: 100px;
-  height: 150px;
-}
+
+
+<style src="../BookCSS/BookCSS.css">
 </style>
   
