0ae55378623166e4b3100604fc9c48f1d1c73c4f
Tich <forshopthing@gmail.com>
2023-08-16 11:43:48 +0800

    暫定

--- EBookStoreVue/src/components/CartAddRemove.vue
+++ EBookStoreVue/src/components/CartAddRemove.vue
@@ -9,7 +9,7 @@
     </div>
 </template>
 <script>
-import { toast } from 'vue3-toastify';
+//import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 export default {
     name: 'CartAddRemove',
